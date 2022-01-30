7b1eee0eb487c56104bd2ea176a4016543a1ddc8
MalekTrabelsi <trabmalek7@gmail.com>
2022-01-30 17:00:34 +0700

    french phone number validation

--- dist/login-validation.js
+++ dist/login-validation.js
@@ -27,6 +27,14 @@ $.validator.addMethod(
         (value.length == 8 && value.match(/(\d{1,2}\-?\d{3}\-?\d{3,4})/))
       );
     }
+    // French Phone
+    else if ($('#country-code-login').val() == "33") {
+      return (
+        this.optional(element) ||
+        (value.length == 9 && value.match(/(\d{1,2}\-?\d{3}\-?\d{3,4})/)) ||
+        (value.length == 10 && value.match(/(\d{1,2}\-?\d{3}\-?\d{3,4})/) && value[0] == "0")
+      );
+    }
   },
   "Please enter a valid phone number"
 );
